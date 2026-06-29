a5a29c33b05bb97653f5fb3f66be17f43771124e
Tommaso De Pascale <tomtom4830@gmail.com>
2026-06-30 01:17:35 +0200

    Fix grasping logic orientation by reverting to arm_tool_link with proper 90 degree pitch

--- src/manipulator_control/src/manipulator_service.cpp
+++ src/manipulator_control/src/manipulator_service.cpp
@@ -36,8 +36,7 @@ public:
   void initialize_moveit(std::shared_ptr<rclcpp::Node> shared_this)
   {
     move_group_arm_ = std::make_shared<moveit::planning_interface::MoveGroupInterface>(shared_this, "arm_torso");
-    move_group_arm_->setEndEffectorLink("gripper_tool_link");
-    // TIAGo MoveIt config usually has higher velocity scaling
+        // TIAGo MoveIt config usually has higher velocity scaling
     move_group_arm_->setMaxVelocityScalingFactor(1.0);
     move_group_arm_->setMaxAccelerationScalingFactor(1.0);
     // Increase planning time slightly
@@ -107,12 +106,14 @@ private:
     // Calculate poses
     const double finger_length = 0.15;
     geometry_msgs::msg::PoseStamped grasp_pose = target_pose;
+    // We command arm_tool_link, which is 'finger_length' higher than the gripper tip.
     grasp_pose.pose.position.z = target_pose.pose.position.z - 0.02 + finger_length;
-    // Orientation: Z axis points into the wrist (down)
-    grasp_pose.pose.orientation.x = 1.0;
-    grasp_pose.pose.orientation.y = 0.0;
+    // Orientation for arm_tool_link to make gripper point DOWN:
+    // X_arm=UP, Z_arm=FORWARD => q=[0, 0.707, 0, 0.707]
+    grasp_pose.pose.orientation.x = 0.0;
+    grasp_pose.pose.orientation.y = 0.70710678;
     grasp_pose.pose.orientation.z = 0.0;
-    grasp_pose.pose.orientation.w = 0.0;
+    grasp_pose.pose.orientation.w = 0.70710678;
 
     // 2. Move directly to grasp pose using free space planning
     RCLCPP_INFO(this->get_logger(), "Planning path to grasp pose...");
@@ -165,11 +166,13 @@ private:
   {
     if (!move_group_arm_) return false;
 
+    const double finger_length = 0.15;
     geometry_msgs::msg::PoseStamped place_pose = target_pose;
-    place_pose.pose.orientation.x = 1.0;
-    place_pose.pose.orientation.y = 0.0;
+    place_pose.pose.position.z += finger_length;
+    place_pose.pose.orientation.x = 0.0;
+    place_pose.pose.orientation.y = 0.70710678;
     place_pose.pose.orientation.z = 0.0;
-    place_pose.pose.orientation.w = 0.0;
+    place_pose.pose.orientation.w = 0.70710678;
 
     // 1. Move directly to place pose using free space planning
     RCLCPP_INFO(this->get_logger(), "Planning path to place pose...");
