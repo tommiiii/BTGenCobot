37aac94d32e8b4d5ac6567579e6ef1a6b48eb33f
Tommaso De Pascale <tomtom4830@gmail.com>
2026-06-30 01:57:48 +0200

    Clean up temporary python scripts

--- test_fk.py
+++
@@ -1,7 +0,0 @@
-import sys
-import rclpy
-from rclpy.node import Node
-import ikpy.chain
-
-# We can't do this easily if ikpy isn't installed here, but the user is running the node inside docker!
-# We can just run docker exec.
--- test_ikpy.py
+++
@@ -1,5 +0,0 @@
-import ikpy.chain
-import numpy as np
-
-# We just need to check ikpy's documentation or signatures
-print(ikpy.chain.Chain.inverse_kinematics.__code__.co_varnames)
